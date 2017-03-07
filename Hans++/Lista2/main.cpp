3cde4fbefdff0db64b47838c9cf138272e14936a
mtszpater <mtszpater@gmail.com>
2017-03-07 07:46:17 +0100

    upd

--- Hans++/Lista2/main.cpp
+++ Hans++/Lista2/main.cpp
@@ -4,33 +4,34 @@
 
 int main( int argc, char* argv [ ] )
 {
-   rational r1( 2, 6 );
-   rational r2( 4, 3 ); 
-   rational r3( 5,6 );
-   rational r4( 1, 2 );
       
    matrix m1 = { { rational(1,2), rational(1,3) }, { rational(-2,7), rational(2,8) } };
    std::cout << m1 << "\n";
 
-   matrix m2 = { { 5,4 }, { 6, rational(1,2) } }; 
+   matrix m2 = { {rational(-1,3), rational(2,7)}, { rational(2,5), rational(-1,7) } }; 
    std::cout << m2 << "\n";
 
-   matrix m3 = { { 4,5}, { -1, 2 }};
+   matrix m3 = { { rational(1,2), rational(-2,7)} , { rational(1,3) , rational(2,8) } };
    
-   std::cout << "\n\n";
-   std::cout << (m1*m2)*m3 << std::endl;
-   std::cout << m1*(m2*m3) << std::endl;
-   
-   std::cout << "\n\n";
-   std::cout << (m1*m2)*rational(1,2) << std::endl;
-   std::cout << m1*(m2*rational(1,2)) << std::endl;
-   
-   std::cout << "\n\n";
-   std::cout << m1.inverse()*m1 << std::endl;
 
-   std::cout << m1. determinant( ) << "\n";
-   std::cout << m1. adjugate( ) << "\n";
-   std::cout << m1. inverse( ) * m1 << "\n";
+   std::cout << "Mnozenie m1,m2 " << "\n" << m1*m2 << std::endl;
+
+   std::cout << "inwersja m3" << std::endl;
+   std::cout << m3 << std::endl;
+   std::cout << m3.inverse() << std::endl;
+   std::cout << m3.determinant() << std::endl;
+
+
+
+   std::cout << "Zadanie 5" << std::endl;
+   std::cout << (m1 * m2) * m3 - m1 * (m2 * m3) << std::endl;
+
+   std::cout << m1*(m2+m3) - (m1*m2+m1*m3) << std::endl;
+   std::cout << (m1+m2)*m3 - (m1*m3+m2*m3) << std::endl;
+
+   std::cout << m1.determinant() * m2.determinant() - (m1*m2).determinant() << std::endl;
+   std::cout << m1*(m1.inverse()) << std::endl;
+   std::cout << m1.inverse()*m1 << std::endl;
 
 }
 
